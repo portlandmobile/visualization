535825b08496a445d5e51406508902f9c853fb8f
Peekay <peekay@Admins-MBP.hsd1.or.comcast.net>
2020-03-30 12:03:37 -0700

    Committer: Peekay <peekay@Admins-MBP.hsd1.or.comcast.net> Changes to be committed: modified:   test_plotly.py

--- test_plotly.py
+++ test_plotly.py
@@ -1,29 +1,40 @@
-import plotly.graph_objects as go
+#Reference from: https://plotly.com/python/bubble-maps/
 
+import plotly.graph_objects as go
 import pandas as pd
+import sys
+
+data_source=sys.argv[1]
 
 df = pd.read_csv('./us_state_lonlat.csv')
+df_data = pd.read_csv(data_source)
 df.head()
 
 df['text'] = df['name'] #+ '<br>Population ' + (df['pop']/1e6).astype(str)+' million'
-limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+#limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+limits = [(0,3000)]
 colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
 cities = []
-scale = 5000
+scale = 10
+
+# Let's combine the data source into the  geolocation source
+df = df.set_index('name')
+df_data =df_data.set_index('name')
+df['data']=df_data  ['data']
 
 fig = go.Figure()
 
 for i in range(len(limits)):
     lim = limits[i]
+    # Breaking the list which is based in the order by population. 
     df_sub = df[lim[0]:lim[1]]
-    print df_sub['text']
     fig.add_trace(go.Scattergeo(
         locationmode = 'USA-states',
         lon = df_sub['lon'],
         lat = df_sub['lat'],
         text = df_sub['text'],
         marker = dict(
-            size = 10,  #df_sub['pop']/scale,
+            size = df_sub['data'].astype(int)/scale, #10,  #df_sub['pop']/scale,
             color = colors[i],
             line_color='rgb(40,40,40)',
             line_width=0.5,
